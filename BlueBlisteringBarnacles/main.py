968d9f8af1f7b272da204eea00ae248469b1b685
nedla2004 <alden@keshap.com>
2017-06-22 21:53:04 -0400

    Update main.py

--- BlueBlisteringBarnacles/main.py
+++ BlueBlisteringBarnacles/main.py
@@ -1,5 +1,5 @@
 import numpy
-print 'This is Python 2'
-print 'Hello, World'
-print 'This is so cool!'
-print numpy.array
+print('This is Python 2')
+print('Hello, World')
+print('This is so cool!')
+print(numpy.array)
